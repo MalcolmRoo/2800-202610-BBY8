79a349a1b18848201640a9546fa53397fbede796
Malcolm Roosdahl <malcoroo@gmail.com>
2026-04-29 15:23:00 -0700

    Camera capture added, need to format for size.

--- src/camera.js
+++ src/camera.js
@@ -1,12 +1,14 @@
 let VIDEO = null;
 let CANVAS = null;
 let CONTEXT = null;
+let CAPTURE = document.getElementById("captureBtn");
 
 function accessCamera() {
     CANVAS = document.getElementById("camCanvas");
     CONTEXT = CANVAS.getContext("2d");
     CANVAS.width = window.innerWidth;
     CANVAS.length = window.innerHeight;
+    
 
     let promise = navigator.mediaDevices.getUserMedia({video:true});
     promise.then(function(signal){
@@ -26,3 +28,18 @@ function updateCanvas() {
     CONTEXT.drawImage(VIDEO, 0, 0);
     window.requestAnimationFrame(updateCanvas);
 }
+
+
+CAPTURE.addEventListener("click", (event) =>{
+    takePicture();
+    event.preventDefault();
+});
+
+
+function takePicture(){
+    CONTEXT.drawImage(VIDEO, 0, 0);//, width, height);
+    console.log("Button pressed");
+    const data = CANVAS.toDataURL("image/png");
+    console.log(data); //data is the image to be passed
+}
+
