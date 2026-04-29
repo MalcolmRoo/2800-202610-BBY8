7be68ac4dfd46caa77c51e23f6aa119e5f90e3c6
Malcolm Roosdahl <malcoroo@gmail.com>
2026-04-29 11:14:50 -0700

    basic camera functionality working

--- src/camera.js
+++ src/camera.js
@@ -4,7 +4,7 @@ let CONTEXT = null;
 
 function accessCamera() {
     CANVAS = document.getElementById("camCanvas");
-    CANVAS = CANVAS.getContext("2d");
+    CONTEXT = CANVAS.getContext("2d");
     CANVAS.width = window.innerWidth;
     CANVAS.length = window.innerHeight;
 
@@ -26,17 +26,3 @@ function updateCanvas() {
     CONTEXT.drawImage(VIDEO, 0, 0);
     window.requestAnimationFrame(updateCanvas);
 }
-
-/*
-CORRESPONDING HTML
-<html>
-    <head>
-        <script src="camera.js"></script>
-        <link href="style.css" rel="stylesheet"/>
-    </head>
-
-    <body onload="accessCamera()">
-        <canvas id="camCanvas"></canvas>
-    </body>
-</html>
-*/
